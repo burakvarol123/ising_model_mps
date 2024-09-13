bfee2b2e6d244071ac53e2b03824cb6c7c477665
burak <burakbabaop@github.com>
2024-09-13 17:42:31 +0200

    fixed hamiltonian

--- isingHamiltonian.jl
+++ isingHamiltonian.jl
@@ -1,27 +1,28 @@
 using ITensors
 
 function ising(
-    h:: Real, 
-    J:: Real, 
+    hdivJ:: Float64, 
+    J::Float64, 
     lattice_size :: Int
     )  
 
     #nearest neigbour
     os = OpSum()
-    for i = 1: lattice_size - 1
+    for i = 1: lattice_size 
         if i+1 < lattice_size
             os += -J,"Sz", i , "Sz", i+1
         end
     end
     #magnetisation
     for i = 1 : lattice_size
-        os += -h, "Sx", i
+        os += -hdivJ * J, "Sx", i
     end 
     #@show os 
     return os 
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
-    h, J , lattice_size = 1, 1 , 5
-    ising(h,J,lattice_size)
+    hdivJ, J , lattice_size = 0.1, 1.0 , 5
+    isingh= ising(hdivJ,J,lattice_size)
+    @show isingh
 end
