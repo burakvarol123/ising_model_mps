9c0a72532d064dd8aebb4c0a6490f6055859fbaf
burak <burakbabaop@github.com>
2024-09-16 14:02:46 +0200

    added comments to evaluator

--- evaluator.jl
+++ evaluator.jl
@@ -1,3 +1,8 @@
+"""
+This script is used for evaluating and plotting physical variables!
+"""
+
+
 using HDF5
 using ITensors
 
@@ -5,10 +10,11 @@ include("./isingHamiltonian.jl")
 
 function magnetization(
     lattice_size:: Int, 
-    psi:: MPS,
-    hdivJ:: Real,
-    J::Real
+    psi:: MPS
     )
+    """
+    Calculate <M_z^2>
+    """
     
     os = OpSum()
     for i = 1 : lattice_size
@@ -24,7 +30,11 @@ end
 function binder_cumulant(
     lattice_size:: Int,
     psi:: MPS,
-)
+    )
+    """
+    Calculate Binder Cumulant. The intersection of binder cumulants of different
+    lengths shows the critical point!
+    """
     os = OpSum()
     for i = 1 : lattice_size
         os += 1/lattice_size, "Sz", i
@@ -41,6 +51,10 @@ function read_data(
     data::String,
     number_states :: Int
     )
+    """
+    Read the wavefunction MPS and energy from dmrg result. Number of states 
+    is denoted as K in the code.
+    """
     f= h5open(data,"r")
     wavefct = []
     for i in 1:number_states
@@ -58,7 +72,10 @@ function make_plot(
     x_data :: Any,
     y_data:: Any,
     savepath:: String,
-)
+    )
+    """
+    Plots the values, I just wrote this for a cleaner code
+    """
     plot(x_data, y_data, marker=:circle)
     xlabel!(xlabel)
     ylabel!(ylabel)
@@ -71,7 +88,7 @@ if abspath(PROGRAM_FILE) == @__FILE__
     using Plots
     #Start evaluating magnetisation, gs energy and energy gap
     
-    data_folder = "/Users/salsa/TensorNetworks/ising_model_mps/data"
+    data_folder = "/Users/salsa/TensorNetworks/ising_model_mps/simulation_data/data"
     files = readdir(data_folder)
     ener = []
     ge = []
@@ -99,34 +116,34 @@ if abspath(PROGRAM_FILE) == @__FILE__
         global counter
         counter += 1
         gs = state[1]
-        magnet = magnetization(lattice_size, gs,hdivJ[counter], 1 )
+        magnet = magnetization(lattice_size, gs )
         push!(magnetisation, magnet)
-        println("finished loop $counter")
+        println("finished loop counter")
     end
-    
+    #Change the saving folder when you will use it
     make_plot("h/J", "Energy", "Ground State Energy, J=1", hdivJ, ge, "/Users/salsa/TensorNetworks/ising_model_mps/visuals/data/energy.png" )
     make_plot("h/J", "Energy Difference", "Ground State vs 1st Excited State, J=1", hdivJ, fe-ge, "/Users/salsa/TensorNetworks/ising_model_mps/visuals/data/energy_difference.png" )
     make_plot("h/J", "<Sum Sz^2>", "Magnetization^2", hdivJ, magnetisation, "/Users/salsa/TensorNetworks/ising_model_mps/visuals/data/magnetisationsz2.png" )
 
+
     #exit() exit if you dont want the binder cumulant. 
-    # Start Binder cumulant, you might have to adjust the file names. I assumed that I always would give multiples of 50 as lattice sizes
-    
+    #Start Binder cumulant, you might have to adjust the file names.
     binder_cumulants = Dict{String, Vector{Any}}()
     hdivJ = 0.0:0.1:2
-    num_datasets = 5
-    for i in 1:num_datasets
-        binder_cumulants["binder_cumulant_$(i*50)"] = []
-        data_folder = "/Users/salsa/TensorNetworks/ising_model_mps/data_$(i*50)"
+    lattice_sizes = 50:50:250
+    for size in lattice_sizes
+        binder_cumulants["binder_cumulant_$size"] = []
+        data_folder = "/Users/salsa/TensorNetworks/ising_model_mps/simulation_data/data_$size"
         files = readdir(data_folder)
         for file in files
             file_path = joinpath(data_folder, file)
             dataset = read_data(file_path, 2)  
             wavefunction = dataset[1]
-            binder = binder_cumulant(i*50, wavefunction[1])
-            push!(binder_cumulants["binder_cumulant_$(i*50)"], binder)
+            binder = binder_cumulant(size, wavefunction[1])
+            push!(binder_cumulants["binder_cumulant_$size"], binder)
         end
         println("Finished:", data_folder)
-        plot!( hdivJ, binder_cumulants["binder_cumulant_$(i*50)"], label= ["lenght = $(i*50)"], legend=:bottomright , marker =:circle)
+        plot!( hdivJ, binder_cumulants["binder_cumulant_$size"], label= ["lenght = $size"], legend=:bottomright , marker =:circle)
         xlabel!("hdivJ")
         ylabel!("U")
         title!("Binder Cumulants")
